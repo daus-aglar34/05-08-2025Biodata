a755bab201bab435ffe46e4de05310242b48a222
daus-aglar34 <prabu_aglar_34rpl@student.smktelkom-mlg.sch.id>
2025-08-06 11:26:55 +0700

    Add files via upload

---
+++ KelasXR6.java
@@ -0,0 +1,15 @@
+public class KelasXR6 {
+    public static void main(String[] args) {
+        System.out.println("Nama : Prabu Panedya Firdaus Putra Aglar");
+        ;
+        System.out.println("Asal Sekolah : SMP Labschool Unesa 3");
+        ;
+        System.out.println("Alamat Rumah : Jl. Manukan Luhur 2K No. 7");
+        ;
+        System.out.println("Jenis Kelamin : Laki-laki");
+        ;
+        System.out.println("Cita-cita : Mobile Apps Developer");
+        ;
+        System.out.println("Hobi : Bermain Game, Basket, Bermain Gitar, Coding");
+    }
+}
